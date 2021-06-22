55b3e679b43cc106c3e4946ff5c9bf2fd5d8c0e0
Christine Tran <ctran97.dev@gmail.com>
2021-06-21 19:03:49 -0700

    convert ember application to react

---
+++ app/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './styles/main.css'
+
+function App(): JSX.Element {
+    return <div className="App">Hello world</div>
+}
+
+export default App
---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import './styles/main.css'
+
+const rootEl = document.getElementById('root')
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    rootEl
+)
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        const NextApp = require('./App').default
+        ReactDOM.render(<NextApp />, rootEl)
+    })
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
