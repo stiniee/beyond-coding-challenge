20fb78a0c46b1ad1a0691845a4f590575331eea2
Christine Tran <ctran97.dev@gmail.com>
2021-06-21 20:40:25 -0700

    Create Listing component

---
+++ src/components/Listing/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './listing.css'
+
+// interface ListingProps {}
+
+const Listing = (): JSX.Element => {
+    return <div>test listing</div>
+}
+
+export default Listing
